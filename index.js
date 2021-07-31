917f8af2c9995754674e0aef4256130780d52bcf
Rodrick Sims <rsimstransport@gmail.com>
2021-07-31 09:21:59 -0400

    all updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: 'YES',
     database: 'Employee_Tracker',
 });
 
